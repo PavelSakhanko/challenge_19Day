df3bf46998b2c295973bf23146c407490bd0622c
Pavel Sakhanko <iospaveldev@gmail.com>
2020-10-19 18:06:38 +0300

    Initial Commit

---
+++ challenge_19Day/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  challenge_19Day
+//
+//  Created by Pavel Sakhanko on 19.10.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ challenge_19Day/challenge_19DayApp.swift
@@ -0,0 +1,17 @@
+//
+//  challenge_19DayApp.swift
+//  challenge_19Day
+//
+//  Created by Pavel Sakhanko on 19.10.20.
+//
+
+import SwiftUI
+
+@main
+struct challenge_19DayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
